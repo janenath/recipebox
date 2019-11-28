e6b48966015dcf30399a945a96e3918533d0cd30
Jane Nath <jane.e.nath@gmail.com>
2019-11-27 16:43:24 -0800

    deploy to heroku

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/recipebox'
 
-mongoose.connect('mongodb://localhost:27017/recipebox', {
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: true
